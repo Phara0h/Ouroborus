ad1f8345d11b0ff64825028271dd5fde8bb37977
Phara0h <jtwhissel@gmail.com>
2020-04-27 19:04:27 -0400

    Init

---
+++ http/src/App.h
@@ -0,0 +1,181 @@
+/*
+ * Authored by Phara0h, 2020.
+ * Intellectual property of third-party.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OUROBORUS_APP_H
+#define OUROBORUS_APP_H
+
+/* An app is a convenience wrapper of some of the most used fuctionalities and allows a
+ * builder-pattern kind of init. Apps operate on the implicit thread local Loop */
+
+#include "HttpContext.h"
+#include "HttpResponse.h"
+#include "PerMessageDeflate.h"
+
+namespace Ouroborus {
+
+template <bool SSL>
+struct TemplatedApp {
+private:
+    /* The app always owns at least one http context, but creates websocket contexts on demand */
+    HttpContext<SSL> *httpContext;
+
+public:
+
+    /* Attaches a "filter" function to track socket connections/disconnections */
+    void filter(fu2::unique_function<void(HttpResponse<SSL> *, int)> &&filterHandler) {
+        httpContext->filter(std::move(filterHandler));
+    }
+
+
+    ~TemplatedApp() {
+        /* Let's just put everything here */
+        if (httpContext) {
+            httpContext->free();
+        }
+    }
+
+    /* Disallow copying, only move */
+    TemplatedApp(const TemplatedApp &other) = delete;
+
+    TemplatedApp(TemplatedApp &&other) {
+        /* Move HttpContext */
+        httpContext = other.httpContext;
+        other.httpContext = nullptr;
+    }
+
+    TemplatedApp(us_socket_context_options_t options = {}) {
+        httpContext = Ouroborus::HttpContext<SSL>::create(Ouroborus::Loop::get(), options);
+    }
+
+    bool constructorFailed() {
+        return !httpContext;
+    }
+
+    TemplatedApp &&get(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("get", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&post(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("post", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&options(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("options", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&del(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("delete", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&patch(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("patch", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&put(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("put", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&head(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("head", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&connect(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("connect", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    TemplatedApp &&trace(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("trace", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    /* This one catches any method */
+    TemplatedApp &&any(std::string pattern, fu2::unique_function<void(HttpResponse<SSL> *, HttpRequest *)> &&handler) {
+        if (httpContext) {
+            httpContext->onHttp("*", pattern, std::move(handler));
+        }
+        return std::move(*this);
+    }
+
+    /* Host, port, callback */
+    TemplatedApp &&listen(std::string host, int port, fu2::unique_function<void(us_listen_socket_t *)> &&handler) {
+        if (!host.length()) {
+            return listen(port, std::move(handler));
+        }
+        handler(httpContext ? httpContext->listen(host.c_str(), port, 0) : nullptr);
+        return std::move(*this);
+    }
+
+    /* Host, port, options, callback */
+    TemplatedApp &&listen(std::string host, int port, int options, fu2::unique_function<void(us_listen_socket_t *)> &&handler) {
+        if (!host.length()) {
+            return listen(port, options, std::move(handler));
+        }
+        handler(httpContext ? httpContext->listen(host.c_str(), port, options) : nullptr);
+        return std::move(*this);
+    }
+
+    /* Port, callback */
+    TemplatedApp &&listen(int port, fu2::unique_function<void(us_listen_socket_t *)> &&handler) {
+        handler(httpContext ? httpContext->listen(nullptr, port, 0) : nullptr);
+        return std::move(*this);
+    }
+
+    /* Port, options, callback */
+    TemplatedApp &&listen(int port, int options, fu2::unique_function<void(us_listen_socket_t *)> &&handler) {
+        handler(httpContext ? httpContext->listen(nullptr, port, options) : nullptr);
+        return std::move(*this);
+    }
+
+    TemplatedApp &&run() {
+        Ouroborus::run();
+        return std::move(*this);
+    }
+
+};
+
+typedef TemplatedApp<false> App;
+typedef TemplatedApp<true> SSLApp;
+
+}
+
+#endif // OUROBORUS_APP_H
---
+++ src/HttpResponseWrapper.h
@@ -0,0 +1,280 @@
+#include "App.h"
+#include "Utilities.h"
+
+#include <v8.h>
+using namespace v8;
+
+struct HttpResponseWrapper {
+
+    template <bool SSL>
+    static inline Ouroborus::HttpResponse<SSL> *getHttpResponse(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = (Ouroborus::HttpResponse<SSL> *) args.Holder()->GetAlignedPointerFromInternalField(0);
+        if (!res) {
+            args.GetReturnValue().Set(isolate->ThrowException(String::NewFromUtf8(isolate, "Invalid access of discarded (invalid, deleted) Ouroborus.HttpResponse/SSLHttpResponse.", NewStringType::kNormal).ToLocalChecked()));
+        }
+        return res;
+    }
+
+    /* Marks this JS object invalid */
+    static inline void invalidateResObject(const FunctionCallbackInfo<Value> &args) {
+        args.Holder()->SetAlignedPointerInInternalField(0, nullptr);
+    }
+
+    /* Takes nothing, kills the connection */
+    template <bool SSL>
+    static void res_close(const FunctionCallbackInfo<Value> &args) {
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            invalidateResObject(args);
+            res->close();
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes function of data and isLast. Expects nothing from callback, returns this */
+    template <bool SSL>
+    static void res_onData(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            /* This thing perfectly fits in with unique_function, and will Reset on destructor */
+            UniquePersistent<Function> p(isolate, Local<Function>::Cast(args[0]));
+
+            res->onData([p = std::move(p), isolate](std::string_view data, bool last) {
+                HandleScope hs(isolate);
+
+                Local<ArrayBuffer> dataArrayBuffer = ArrayBuffer::New(isolate, (void *) data.data(), data.length());
+
+                Local<Value> argv[] = {dataArrayBuffer, Boolean::New(isolate, last)};
+                CallJS(isolate, Local<Function>::New(isolate, p), 2, argv);
+
+                NeuterArrayBuffer(dataArrayBuffer);
+            });
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes nothing, returns nothing. Cb wants nothing returned. */
+    template <bool SSL>
+    static void res_onAborted(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            /* This thing perfectly fits in with unique_function, and will Reset on destructor */
+            UniquePersistent<Function> p(isolate, Local<Function>::Cast(args[0]));
+
+            /* This is how we capture res (C++ this in invocation of this function) */
+            UniquePersistent<Object> resObject(isolate, args.Holder());
+
+            res->onAborted([p = std::move(p), resObject = std::move(resObject), isolate]() {
+                HandleScope hs(isolate);
+
+                /* Mark this resObject invalid */
+                Local<Object>::New(isolate, resObject)->SetAlignedPointerInInternalField(0, nullptr);
+
+                CallJS(isolate, Local<Function>::New(isolate, p), 0, nullptr);
+            });
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes nothing, returns arraybuffer */
+    template <bool SSL>
+    static void res_getRemoteAddress(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            std::string_view ip = res->getRemoteAddress();
+
+            /* Todo: we need to pass a copy here */
+            args.GetReturnValue().Set(ArrayBuffer::New(isolate, (void *) ip.data(), ip.length()/*, ArrayBufferCreationMode::kInternalized*/));
+        }
+    }
+
+    /* Returns the current write offset */
+    template <bool SSL>
+    static void res_getWriteOffset(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            args.GetReturnValue().Set(Integer::New(isolate, getHttpResponse<SSL>(args)->getWriteOffset()));
+        }
+    }
+
+    /* Takes function of bool(int), returns this */
+    template <bool SSL>
+    static void res_onWritable(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            /* This thing perfectly fits in with unique_function, and will Reset on destructor */
+            UniquePersistent<Function> p(isolate, Local<Function>::Cast(args[0]));
+
+            res->onWritable([p = std::move(p), isolate](int offset) -> bool {
+                HandleScope hs(isolate);
+
+                Local<Value> argv[] = {Integer::NewFromUnsigned(isolate, offset)};
+
+                /* We should check if this is really here! */
+                MaybeLocal<Value> maybeBoolean = CallJS(isolate, Local<Function>::New(isolate, p), 1, argv);
+                if (maybeBoolean.IsEmpty()) {
+                    std::cerr << "ERROR! onWritable must return a boolean value according to documentation!" << std::endl;
+                    exit(-1);
+                }
+
+                return BooleanValue(isolate, maybeBoolean.ToLocalChecked());
+                /* How important is this return? */
+            });
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes string or arraybuffer, returns this */
+    template <bool SSL>
+    static void res_writeStatus(const FunctionCallbackInfo<Value> &args) {
+        auto *res = getHttpResponse<SSL>(args);
+            if (res) {
+            NativeString data(args.GetIsolate(), args[0]);
+            if (data.isInvalid(args)) {
+                return;
+            }
+            res->writeStatus(data.getString());
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes string or arraybuffer, returns this */
+    template <bool SSL>
+    static void res_end(const FunctionCallbackInfo<Value> &args) {
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            NativeString data(args.GetIsolate(), args[0]);
+            if (data.isInvalid(args)) {
+                return;
+            }
+            invalidateResObject(args);
+            res->end(data.getString());
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes data and optionally totalLength, returns true for success, false for backpressure */
+    template <bool SSL>
+    static void res_tryEnd(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            NativeString data(args.GetIsolate(), args[0]);
+            if (data.isInvalid(args)) {
+                return;
+            }
+
+            int totalSize = 0;
+            if (args.Length() > 1) {
+                totalSize = args[1]->Uint32Value(isolate->GetCurrentContext()).ToChecked();
+            }
+
+            auto [ok, hasResponded] = res->tryEnd(data.getString(), totalSize);
+
+            /* Invalidate this object if we responded completely */
+            if (hasResponded) {
+                invalidateResObject(args);
+            }
+
+            /* This is a quick fix, it will need updating in Ouroborus later on */
+            Local<Array> array = Array::New(isolate, 2);
+            array->Set(isolate->GetCurrentContext(), 0, Boolean::New(isolate, ok)).ToChecked();
+            array->Set(isolate->GetCurrentContext(), 1, Boolean::New(isolate, hasResponded)).ToChecked();
+
+            args.GetReturnValue().Set(array);
+        }
+    }
+
+    /* Takes data, returns true for success, false for backpressure */
+    template <bool SSL>
+    static void res_write(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            NativeString data(args.GetIsolate(), args[0]);
+            if (data.isInvalid(args)) {
+                return;
+            }
+            bool ok = res->write(data.getString());
+
+            args.GetReturnValue().Set(Boolean::New(isolate, ok));
+        }
+    }
+
+    /* Takes key, value. Returns this */
+    template <bool SSL>
+    static void res_writeHeader(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+            NativeString header(args.GetIsolate(), args[0]);
+            if (header.isInvalid(args)) {
+                return;
+            }
+            NativeString value(args.GetIsolate(), args[1]);
+            if (value.isInvalid(args)) {
+                return;
+            }
+            res->writeHeader(header.getString(),value.getString());
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    /* Takes function, returns this */
+    template <bool SSL>
+    static void res_cork(const FunctionCallbackInfo<Value> &args) {
+        Isolate *isolate = args.GetIsolate();
+        auto *res = getHttpResponse<SSL>(args);
+        if (res) {
+
+            res->cork([cb = Local<Function>::Cast(args[0]), isolate]() {
+                /* This one is called from JS so we don't need CallJS */
+                cb->Call(isolate->GetCurrentContext(), isolate->GetCurrentContext()->Global(), 0, nullptr).IsEmpty();
+            });
+
+            args.GetReturnValue().Set(args.Holder());
+        }
+    }
+
+    template <bool SSL>
+    static Local<Object> init(Isolate *isolate) {
+        Local<FunctionTemplate> resTemplateLocal = FunctionTemplate::New(isolate);
+        if (SSL) {
+            resTemplateLocal->SetClassName(String::NewFromUtf8(isolate, "Ouroborus.SSLHttpResponse", NewStringType::kNormal).ToLocalChecked());
+        } else {
+            resTemplateLocal->SetClassName(String::NewFromUtf8(isolate, "Ouroborus.HttpResponse", NewStringType::kNormal).ToLocalChecked());
+        }
+        resTemplateLocal->InstanceTemplate()->SetInternalFieldCount(1);
+
+        /* Register our functions */
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "writeStatus", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_writeStatus<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "end", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_end<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "tryEnd", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_tryEnd<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "write", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_write<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "writeHeader", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_writeHeader<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "close", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_close<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "getWriteOffset", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_getWriteOffset<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "onWritable", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_onWritable<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "onAborted", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_onAborted<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "onData", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_onData<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "getRemoteAddress", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_getRemoteAddress<SSL>));
+        resTemplateLocal->PrototypeTemplate()->Set(String::NewFromUtf8(isolate, "cork", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, res_cork<SSL>));
+
+        /* Create our template */
+        Local<Object> resObjectLocal = resTemplateLocal->GetFunction(isolate->GetCurrentContext()).ToLocalChecked()->NewInstance(isolate->GetCurrentContext()).ToLocalChecked();
+        
+        return resObjectLocal;
+    }
+};
